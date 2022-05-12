5d5904fec74f91ecbd2273a531c39ec593d47d4a
kamilchulakov <mr1english4@gmail.com>
2022-05-12 13:21:03 +0300

    Add ktor deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,16 @@ plugins {
 group = "me.user"
 version = "1.0-SNAPSHOT"
 
+val ktor_version = "2.0.1"
+
 repositories {
     mavenCentral()
 }
 
-
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+}
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
