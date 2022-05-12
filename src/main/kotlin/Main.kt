743c9de2ece6a16457a10ba34fb5f4ffa3a05cd4
kamilchulakov <mr1english4@gmail.com>
2022-05-12 13:40:09 +0300

    Add channel processing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,42 @@
 import db.execute
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import ql.QLExpressionParser
 import java.io.FileNotFoundException
+import kotlin.system.exitProcess
 
 fun main() {
-    println("> ")
-    var line = readln()
-    while (line.isNotBlank() ) {
-        try {
-            execute(QLExpressionParser(line).parseTerminate())
-        } catch (ex: FileNotFoundException) {
-            println("File not found.")
+    runBlocking {
+        val receiveChannel = Channel<String>()
+        launch(Dispatchers.IO) {
+            while (true) {
+                val answer = receiveChannel.receive()
+                if (answer != "EXIT") {
+                    try {
+                        execute(QLExpressionParser(answer).parseTerminate())
+                    } catch (ex: FileNotFoundException) {
+                        println("Server closed a connection: $ex")
+                        receiveChannel.close()
+                        exitProcess(0)
+                    }
+                } else {
+                    println("Server closed a connection")
+                    receiveChannel.close()
+                    exitProcess(0)
+                }
+            }
         }
-        print("> ")
-        line = readln()
+
+        println("> ")
+        var line = readlnOrNull()
+        while (!line.isNullOrBlank()) {
+            receiveChannel.send(line)
+            print("> ")
+            line = readln()
+        }
+        receiveChannel.send("EXIT")
     }
 }
 
