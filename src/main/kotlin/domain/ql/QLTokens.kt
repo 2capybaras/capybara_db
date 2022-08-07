ba1da1af2749e61aa4c469ad5b504636698fed74
kamilchulakov <mr1english4@gmail.com>
2022-08-08 02:08:27 +0300

    Add better tokens #10

--- src/main/kotlin/domain/ql/QLTokens.kt
+++ src/main/kotlin/domain/ql/QLTokens.kt
@@ -1,16 +1,20 @@
 package domain.ql
 
-import domain.model.Table
+import kotlin.Int.Companion.MAX_VALUE
 
 sealed interface QLToken
-
-open class QLTerminate: QLToken
-
-open class QLData(val data: Table): QLToken
-
-class QLTableName(val name: String): QLToken
-
-class QLRange(val a: Int = 0, val b: Int = Int.MAX_VALUE): QLToken
-
-class QLSelect(val tableName: QLTableName, val range: QLRange?): QLTerminate()
+open class QLDefaultValue<T>(val value: T): QLToken
+object QLAnyString: QLToken
+class QLString(val equal: String): QLToken
+object QLAnyInt: QLToken
+class QLInt(defaultValue: Int): QLDefaultValue<Int>(defaultValue)
+open class QLDependable(val dependencies: List<QLToken>): QLToken
+open class QLOptionalDependable(vararg dependencies: QLToken): QLDependable(dependencies.toList())
+open class QLOneFromOptional(vararg dependencies: QLToken): QLOptionalDependable(*dependencies)
+open class QLStart(vararg dependencies: QLToken): QLDependable(dependencies.toList())
+open class QLRange(vararg dependencies: QLToken): QLDependable(dependencies.toList())
+object QLFromToRange: QLRange(QLString("FROM"), QLInt(0), QLString("TO"), QLInt(MAX_VALUE))
+object QLIndexRange: QLRange(QLString("INDEX"), QLAnyInt)
+object QLAnyRange: QLOneFromOptional(QLFromToRange, QLIndexRange)
+object QLSelect: QLStart(QLString("SELECT"), QLString("FROM"), QLAnyString, QLAnyRange)
 
